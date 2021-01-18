86c1c42ea7215421b059acc3a71e637b8c11fbe4
叶同学 <jinzhanye9@gmail.com>
2021-01-18 20:25:57 +0800

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cookieSession = require('cookie-session')
+const app = express()
+
+app.use(cookieSession({
+    name: 'my-session',
+    // 随机生成密钥
+    secret: '7EjfQ0tbntWr380eHytPMZcydC1e3WKD',
+    maxAge: 1000 * 3600 * 24 * 20,
+  }))
+
+app.get('/', (req, res) => {
+  // btoa(JSON.stringify({ tokenA: 'foo', tokenB: 'bar' })) === 'eyJ0b2tlbkEiOiJmb28iLCJ0b2tlbkIiOiJiYXIifQ=='
+  req.session.tokenA = 'foo'
+  req.session.tokenB = 'bar'
+
+  res.send(' I am home')
+})
+
+app.listen(3900, () => {
+  console.log('I am listening port 3900...');
+});
