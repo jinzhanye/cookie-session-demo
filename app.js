7fdfeac0ab05052aaa7318c61f18c14aa36028e5
叶同学 <jinzhanye9@gmail.com>
2021-01-23 18:31:26 +0800

    init

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const cookieSession = require('cookie-session')
 const app = express()
 
 app.use(cookieSession({
-    name: 'my-session',
-    // 随机生成密钥
-    secret: '7EjfQ0tbntWr380eHytPMZcydC1e3WKD',
-    maxAge: 1000 * 3600 * 24 * 20,
-  }))
+  name: 'my-session',
+  // 随机生成密钥
+  secret: '7EjfQ0tbntWr380eHytPMZcydC1e3WKD',
+  maxAge: 1000 * 3600 * 24 * 20,
+}))
 
 app.get('/', (req, res) => {
   // btoa(JSON.stringify({ tokenA: 'foo', tokenB: 'bar' })) === 'eyJ0b2tlbkEiOiJmb28iLCJ0b2tlbkIiOiJiYXIifQ=='
